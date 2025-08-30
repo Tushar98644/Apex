92bf32637936d9ed5b99288a4f5cc5e50631e4d9
Tushar Banik <b.toshar@iitg.ac.in>
2025-08-30 21:38:05 +0530

    feat: add confi

--- api_server/app/main.py
+++ api_server/app/main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from core.config import settings
 
 app = FastAPI(
     title="Apex",
     description="C++ Trading Engine",
     version="0.1.0",
     docs_url="/docs",
-    redoc_url="/redcocs"
+    redoc_url="/redoc"
 )
 
 app.add_middleware(
     CORSMiddleware, 
-    allow_origins=["*"],
+    allow_origins=settings.ALLOWED_ORIGINS,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
